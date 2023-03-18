a61222078bce9e8c4fa487b0cab14ad725aa518a
Muhammad Faisal A <sidiqfaisal30@gmail.com>
2023-03-18 13:48:20 +0700

    modify file url

--- url/url.go
+++ url/url.go
@@ -10,7 +10,7 @@ import (
 func Web(page *fiber.App) {
 	page.Post("/api/whatsauth/request", controller.PostWhatsAuthRequest)  //API from user whatsapp message from iteung gowa
 	page.Get("/ws/whatsauth/qr", websocket.New(controller.WsWhatsAuthQR)) //websocket whatsauth
-	page.Get("/", controller,GetHome) //websocket whatsauth
-	page.Get("/faisal", controller, GetHome) //websocket whatsauth
+	// page.Get("/", controller,GetHome) //websocket whatsauth
+	// page.Get("/faisal", controller, GetHome) //websocket whatsauth
 
 }
