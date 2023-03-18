c764b607d4ee28667da661124033c3d247df6615
Muhammad Faisal A <sidiqfaisal30@gmail.com>
2023-03-18 13:41:50 +0700

    modify file controller

--- controller/controller.go
+++ controller/controller.go
@@ -32,7 +32,7 @@ func GetHome(c *fiber.Ctx) error{
 	return c.JSON("Hello World")
 }
 
-func GetdataFaisal(c *fiber.Ctx) error{
-	getip := faisal.GetDataAllbyStatus("active", config.MongoConn, "data_compllain")
-	return c.JSON(getip)
-}
+// func GetdataFaisal(c *fiber.Ctx) error{
+// 	getip := faisal.GetDataAllbyStatus("active", config.MongoConn, "data_compllain")
+// 	return c.JSON(getip)
+// }
